43596eb20bb67f37a367177853052e94a5c59f77
alex <agarci9@xtec.cat>
2024-04-08 10:21:27 +0200

    Upload Task 1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //
+    public function index(int $user_id) : view
+    {
+        //dd(User::all());
+        $user =User::find($user_id);
+
+        return view('welcome', ['user' => $user]);
+    }
+}
